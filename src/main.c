b64158f6e4d56f15c2bd3e646c60540baef7a995
Isac's Desktop <iwandiba@hotmail.com>
2023-12-17 02:02:59 -0500

    Add project files.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+﻿#include <SDL.h>
+
+#define SDL_MAIN_HANDLED
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("Failure %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Log("Success\n");
+    SDL_Quit();
+    return 0;
+}
