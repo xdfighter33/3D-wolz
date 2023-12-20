944b63ccd0d7c77cdac9d0779aac49b6aa085913
Isac's Desktop <iwandiba@hotmail.com>
2023-12-20 01:51:34 -0500

    Making project more legible
    Made and defined functions for display

---
+++ src/display.c
@@ -0,0 +1,95 @@
+﻿#include "display.h"
+
+
+/* Declare Variables */
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+
+int window_width = 800;
+int window_height = 600;
+
+/* Setting up functions */
+bool initialize_window(void){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        fprintf(stderr, "Error Initialzing SDL\n");
+        return false; 
+    }
+window = SDL_CreateWindow(
+NULL,
+SDL_WINDOWPOS_CENTERED,
+SDL_WINDOWPOS_CENTERED,
+window_width,
+window_height,
+SDL_WINDOW_BORDERLESS);
+if (!window){
+    fprintf(stderr, "Error creating the window \n");
+    return false; 
+}
+
+renderer = SDL_CreateRenderer(window, -1, 0);
+if(!renderer){
+    fprintf(stderr, "Erroe creating the SDL render \n");
+    return false; 
+}
+
+return true; 
+}
+
+
+
+
+
+void draw_grid(void){
+for(int y = 0; y < window_height; y+= 10){
+    for(int x = 0; x < window_width; x+= 10){
+      
+color_buffer[(window_width * y) + x] =  0xFF444444;
+        
+    }
+}
+}
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int current_x = x + i;
+            int current_y = y + j;
+            color_buffer[(window_width * current_y) + current_x] = color;
+        }
+    }
+}
+    
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[(window_width * y) + x] = color;
+        }
+
+    }
+}
+
+
+
+
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ src/display.h
@@ -0,0 +1,40 @@
+#include <SDL.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#pragma once 
+#ifndef DISPLAY_H 
+#define DISPLAY_HG
+
+
+
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+
+
+extern uint32_t* color_buffer;
+extern SDL_Texture* color_buffer_texture;
+
+
+extern int window_width;
+extern int window_height;
+
+extern bool initialize_window(void);
+
+
+extern void draw_grid(void);
+
+extern void draw_rect(int x, int y, int width, int height, uint32_t color);
+
+
+extern void render_color_buffer(void);
+
+
+extern void clear_color_buffer(uint32_t color);
+
+
+extern void destroy_window(void);
+
+
+#endif 
+
