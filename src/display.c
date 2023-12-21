2fe245e8b6864dbbb758c2b6e96b818b52ca247f
Isac's Desktop <iwandiba@hotmail.com>
2023-12-21 18:32:42 -0500

    Fixed smiley_face to say Line now
    Fixed drawl_pixel to only draw in bounds of screen
    
    Abstracted Draw_RECT with draw pixel

--- src/display.c
+++ src/display.c
@@ -53,20 +53,26 @@ color_buffer[(window_width * y) + x] =  0xFF444444;
 }
 }
 
-void smiley_face(int x, int y,int length,uint32_t color) {
-    for (int i = 0; i < length; i++) {
-        int current_x = x + i;
-        color_buffer[(window_width * y) + current_x] = color;
+void draw_pixel(int x, int y, uint32_t color) {
+    if (x >= 0 && x < window_width && y >= 0 && y < window_height) {
+        color_buffer[(window_width * y) + x] = color;
     }
 }
 
+
+void draw_line(int x, int y,int length,uint32_t color) {
+    for (int i = 0; i < length; i++) {
+        int current_x = x + i; 
+        draw_pixel(current_x, y, color);
+    }
+}
 
 void draw_rect(int x, int y, int width, int height, uint32_t color) {
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
             int current_x = x + i;
             int current_y = y + j;
-            color_buffer[(window_width * current_y) + current_x] = color;
+            draw_pixel(current_x, current_y, color);
         }
     }
 }
