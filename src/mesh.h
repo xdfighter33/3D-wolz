da1f77448bbee70769a8e2a7891595397d4d0087
Isac's Desktop <iwandiba@hotmail.com>
2023-12-24 08:09:08 -0500

    ADDED PYRAMID VERTICES FACES AND MESH DATA

--- src/mesh.h
+++ src/mesh.h
@@ -4,15 +4,28 @@
 
 #include "vector.h"
 #include "triangle.h"
+#include "array.h"
 #pragma once
+#define N_PYRAMID_VERTICES 5
+#define N_PYRAMID_FACES 4
+#define N_CUBE_VERTICES 8
+#define N_CUBE_FACES (6 * 2 )
+extern vec3_t cube_vertices[N_CUBE_VERTICES];
+extern vec3_t pyramid_vertices[N_PYRAMID_VERTICES];
 
-#define N_MESH_VERTICES 8
-extern vec3_t mesh_vertices[N_MESH_VERTICES];
+extern face_t cube_faces[N_CUBE_FACES];
+extern face_t pyramid_face[N_PYRAMID_FACES];
 
 
+typedef struct {
+	vec3_t* vertices;
+	face_t* faces;
+	vec3_t rotation;
 
-#define N_MESH_FACES (6 * 2 )
-extern face_t mesh_faces[N_MESH_FACES];
+}mesh_t;
 
+extern mesh_t mesh;
 
+void load_cube_mesh_data(void);
+void load_pyramid_mesh_data(void);
 // TODO: Reference additional headers your program requires here.
