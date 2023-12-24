24a4a10aafcb3fee11a1b7ef81b868db243b6722
Isac's Desktop <iwandiba@hotmail.com>
2023-12-24 08:09:24 -0500

    made function for mesh data

--- src/mesh.c
+++ src/mesh.c
@@ -1,6 +1,25 @@
+#include <stdio.h>
 #include "mesh.h"
+#include "array.h"
 
-vec3_t mesh_vertices[N_MESH_VERTICES] = {
+
+mesh_t mesh = {
+	.vertices = NULL,
+	.faces = NULL,
+	.rotation = {0,0,0}
+};
+
+vec3_t pyramid_vertices[N_PYRAMID_VERTICES] = {
+	{.x = -1, .y = -1, .z = -1 }, //1 
+	{.x = -1, .y = 1, .z = -1 }, //2 
+	{.x = 1, .y = 1, .z = -1 }, //3 
+	{.x = 1, .y = -1, .z = -1 }, //4
+
+	//APEX OF THE TRIANGLE 
+	{.x = 0, .y = 0, .z = 1}
+};
+
+vec3_t cube_vertices[N_CUBE_VERTICES] = {
 	{.x = -1, .y = -1, .z = -1 }, //1 
 	{.x = -1, .y = 1, .z = -1 }, //2 
 	{.x = 1, .y = 1, .z = -1 }, //3 
@@ -13,7 +32,7 @@ vec3_t mesh_vertices[N_MESH_VERTICES] = {
 
 };
 
-face_t mesh_faces[N_MESH_FACES] = {
+face_t cube_faces[N_CUBE_FACES] = {
 
 	// Front
 	{.a = 1, .b = 2, .c = 3},
@@ -37,3 +56,37 @@ face_t mesh_faces[N_MESH_FACES] = {
 	{.a = 6, .b = 1, .c = 4},
 
 };
+
+face_t pyramid_faces[N_PYRAMID_FACES] = {
+	{.a = 1, .b = 2, .c = 5},
+
+	{.a =2, .b = 3, .c = 5},
+
+	{.a =3, .b = 4, .c = 5},
+
+	{.a =4, .b = 1, .c = 5},
+};
+
+
+void load_cube_mesh_data(void) {
+	for (int i = 0; i < N_CUBE_VERTICES; i++) {
+		vec3_t cube_vertex = cube_vertices[i];
+		array_push(mesh.vertices, cube_vertex);
+	}
+	for (int i = 0; i < N_CUBE_FACES; i++) {
+		face_t cube_face = cube_faces[i];
+		array_push(mesh.faces, cube_face);
+	}
+}
+
+
+void load_pyramid_mesh_data(void) {
+	for (int i = 0; i < N_PYRAMID_VERTICES; i++) {
+		vec3_t pyramid_vertex = pyramid_vertices[i];
+		array_push(mesh.vertices, pyramid_vertex);
+	}
+	for (int i = 0; i < N_PYRAMID_FACES; i++) {
+		face_t pyramid_face = pyramid_faces[i];
+		array_push(mesh.faces, pyramid_face);
+	}
+}
