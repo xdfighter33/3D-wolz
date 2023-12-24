ff0df947d2578170798de6396dcc404b76c94d14
Isac's Desktop <iwandiba@hotmail.com>
2023-12-24 08:08:24 -0500

    Began to add ENUM for colors

--- src/display.h
+++ src/display.h
@@ -8,7 +8,15 @@
 #define FPS 30
 #define FRAME_TIME_TARGET (1000/ FPS)
 
-
+/*enum colors {
+	RED,
+	GREEN,
+	BLUE,
+	WHITE,
+	BLACK
+};
+ADDD HASHMAP TO PROGRAM
+*/
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
 
@@ -28,12 +36,12 @@ extern void draw_grid(void);
 extern void draw_rect(int x, int y, int width, int height, uint32_t color);
 
 extern void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+
 extern void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 
 extern void draw_pixel(int x, int y, uint32_t color);
 
 extern void render_color_buffer(void);
-
 
 extern void clear_color_buffer(uint32_t color);
 
