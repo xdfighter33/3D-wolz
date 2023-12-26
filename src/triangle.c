acd34ddaa0817ac079a26fd40007e7565cd521e8
Isac's Desktop <iwandiba@hotmail.com>
2023-12-25 20:18:46 -0500

    Adding triangle Midpoint

--- src/triangle.c
+++ src/triangle.c
@@ -1,2 +1,51 @@
 #include "triangle.h"
+#include "display.h"
 
+void fill_float_bottom_triangle(int x0, int y0, int x1,int y1,int x2,int y2, uint32_t color) {
+	float inv_slope_1 = (float)(x1 - x0) / (y1 - y0);
+	float inv_slope_2 = (float)(x2 - x0) / (y2 - y0);
+
+	float x_start = x0;
+	float x_end = x0;
+	for (int y = y0; y <= y2; y++) {
+		draw_line(x_start, y, x_end, y, color);
+		x_start += inv_slope_1;
+		x_end += inv_slope_2;
+	}
+
+}
+
+//void fill_float_top_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+
+//}
+
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+void draw_filled_traingle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+	if (y0 > y1) {
+		swap(&y0, &y1);
+		swap(&x0, &x1);
+	}
+	if (y1 > y2){
+		swap(&y1, &y2);
+		swap(&x1, &x2);
+	}
+	if (y0 > y1) {
+		swap(&y0, &y1);
+		swap(&x0, &x1);
+	}
+
+	int mY = y1;
+	int mX = ((float)((x2 - x0) * (y1 - y0)) /(float) (y2 - y0)) + x0;
+	
+	fill_float_bottom_triangle(x0, y0, x1, y1, mX, mY,color);
+
+
+
+	//fill_float_top_triangle(x1,y1, mX,mY, x2,y2,color);
+
+
+}
--- src/triangle.h
+++ src/triangle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdlib.h>
 #include "vector.h"
 typedef struct {
 	int a;
@@ -9,3 +10,7 @@ typedef struct {
 typedef struct {
 	vec2_t points[3];
 }triangle_t ;
+
+extern void fill_float_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int m2, uint32_t color);
+//void fill_float_bottom_triangle(int x0, int y0, int x1, int y1, int x2, int m2, uint32_t color);
+extern void draw_filled_traingle(int x0, int y0, int x1, int y1, int y2, uint32_t color);
