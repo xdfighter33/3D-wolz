8838d92ccce797d5e570703e4088ebc2b5a3fb52
Isacs Macbook <Iwandiba@yahoo.com>
2023-12-31 15:51:34 -0500

    Setting up basic lighitng

---
+++ src/light.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "vector.h"
+
+typedef struct {
+    vec3_t direction;
+
+} light_t;
+
+extern light_t light; 
+
+
+uint32_t light_apply_intensity(uint32_t original_color, float percentage_factor);
